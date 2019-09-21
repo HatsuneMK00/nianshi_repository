0e16387d08d7e324e6cc5f26eee1a265813e259f
GodYihan <hyh739146825@gmail.com>
2019-09-21 16:12:19 +0800

    collection fixed

--- nianshi/pages/collection/collection.js
+++ nianshi/pages/collection/collection.js
@@ -79,7 +79,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    var page = getCurrentPages().pop(); //当前页面
+    if (page == undefined || page == null) return;
+    page.onLoad();
   },
 
   /**
